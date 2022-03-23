a64f2ada7ac3ce006dc074707363898aef79e304
Chleo <cproesch@student.42.fr>
2022-03-23 16:10:57 +0100

    added absolute values for boxes coordinates

--- sources/calculations.c
+++ sources/calculations.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   calculations.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: avan-bre <avan-bre@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cproesch <cproesch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 15:14:38 by avan-bre          #+#    #+#             */
-/*   Updated: 2022/03/23 14:52:55 by avan-bre         ###   ########.fr       */
+/*   Updated: 2022/03/23 16:09:50 by cproesch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,37 @@ double	hit(t_map *map, t_player *player, int i)
 {
 	t_vector	vecX;
 	t_vector	vecY;
+	int			box_x;
+	int			box_y;
 
 	// vecX[0] = floor((player->pos[0] + 1);
 	vecX[0] = player->pos[0] + (player->sdX * cos(player->ray_angle));
 	vecX[1] = player->pos[1] + (player->sdX * sin(player->ray_angle));
 	vecY[0] = player->pos[0] + (player->sdY * cos(player->ray_angle));
 	vecY[1] = player->pos[1] + (player->sdY * sin(player->ray_angle));
-	printf("vecX[0] = %f, vecX[1] = %f, vecY[0] = %f, vecY[1] = %f\n", vecX[0], vecX[1], vecY[0], vecY[1]);
+	// if ((player->ray_angle < PI / 2) || (player->ray_angle > 3 * PI / 2))
+		box_x = abs((int)vecX[0]);
+	// else
+		// box_x = abs((int)vecX[0]) - 1;
+	box_y = abs((int)vecX[1]);
+	printf("vecX[0] = %f et map->width = %d, vecX[1] = %f et map->height = %d\n", vecX[0], map->width, vecX[1], map->height);
+	printf("vecY[0] = %f, vecY[1] = %f\n", vecY[0], vecY[1]);
 	printf("player->ray_angle = %f\n", player->ray_angle);
-	if (vecX[0] < map->width && vecX[1] < map->height 
-		&& map->map[(int)vecX[1]][(int)vecX[0]] == '1')
+	if (fabs(vecX[0]) < (map->width - 1) && fabs(vecX[1]) < (map->height - 1)
+		&& map->map[box_y][box_x] == '1')
 	{
 		printf("hit!\n");
 		player->next_hit[i][0] = (int)vecX[0];		// a supprimer 
 		player->next_hit[i][1] = vecX[1];		// a supprimer 
 		return (player->sdX);
 	}
-	if (vecY[0] < map->width && vecY[1] < map->height 
-		&& map->map[(int)vecY[1]][(int)vecY[0]] == '1')
+	box_x = abs((int)vecY[0]);
+	// if (player->ray_angle < PI)
+		box_y = abs((int)vecY[1]);
+	// else
+		// box_y = abs((int)vecY[1]) - 1;
+	if (fabs(vecY[0]) < map->width && fabs(vecY[1]) < map->height 
+		&& map->map[box_y][box_x] == '1')
 	{
 		printf("hit!\n");
 		player->next_hit[i][0] = vecY[0];		// a supprimer 
@@ -70,7 +83,7 @@ void	get_view_points(t_player *player, t_map *map)
 	// while (player->ray_angle - player->angle < (player->plane * 2))
 	{
 		printf("kikou\n");
-		if ((player->ray_angle < PI / 2) || (player->ray_angle < 3 * PI / 2))
+		if ((player->ray_angle < PI / 2) || (player->ray_angle > 3 * PI / 2))
 			player->sdX = floor((player->pos[0] + 1 - player->pos[0]) / cos(player->ray_angle));
 		else if ((player->ray_angle != PI / 2) && (player->ray_angle != 3 * PI / 2))
 			player->sdX = floor((player->pos[0] - player->pos[0]) / cos(player->ray_angle));
